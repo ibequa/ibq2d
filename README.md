5dc74a987076256a8c10a146fc48e2f6fe236063
Ilya <ibequa@gmail.com>
2015-10-26 04:39:40 +0300

    updated readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# ibq2d
+2d game engine in development;
+currently implemented:
+    - keyboard input
+    - collisions
--- src/com/ibq2d/engine/Core.java
+++ src/com/ibq2d/engine/Core.java
@@ -24,7 +24,7 @@ public class Core {
     public void start() {
         if (isRunning)
             return;
-        
+
         run();
     }
 
