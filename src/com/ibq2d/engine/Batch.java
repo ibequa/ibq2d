b8c4ef676349a277ca87496d67bf5e760f4f3301
Ilya <ibequa@gmail.com>
2015-11-06 17:35:02 +0300

    Update Batch.java

--- src/com/ibq2d/engine/Batch.java
+++ src/com/ibq2d/engine/Batch.java
@@ -1,6 +1,6 @@
 package com.ibq2d.engine;
 
 public interface Batch {
-    void draw(Texture texture, float x, float y); // x, y - bottom left coord. of drawn rect
+    void draw(Texture texture, float x, float y);
     void draw(Texture texture, float x, float y, float scaleX, float scaleY, float rotation);
 }
